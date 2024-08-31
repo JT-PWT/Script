03f4076416a2a2ae90e70be82884c2a9435b5fb1
Jeferson <jeferson.silva220@gmail.com>
2024-08-30 21:30:49 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,35 @@
+//Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores no console
+
+const idade=51;
+console.log("Minha idade é " + idade + ".");
+
+const peso=36;
+console.log("Meu peso é " + peso + ".");
+
+const cidade="Resende";
+console.log("Minha cidade é " + cidade + ".");
+
+const bairro="Cabral";
+console.log("Meu bairro é " + bairro + ".");
+
+const empresa="Powertrain";
+console.log("Trabalho na empresa " + empresa + ".");
+
+const Rua="Domingos Storino";
+console.log("Minha rua é " + Rua + ".");
+
+const número=756;
+console.log("O número da minha casa é " + número + ".");
+
+const hobby="pescar";
+console.log("Meu hobby é " + hobby + "." + " Não é mentira!!!");
+
+const bebida="pinga";
+console.log("Eu gosto de " + bebida + "." + " 51");
+
+const casado="Rô";
+console.log("Sou casado com a " + casado + ".")
+
+console.log("Minha idade é "+idade+", meu peso é "+peso+", minha cidade é "+cidade+", meu bairro é "+bairro+", trabalho na empresa "+empresa+", minha rua é "+Rua+"nº "+número+". Meu hobby é "+hobby+". Gosto de tomar "+bebida+" e sou casado com a "+casado+".")
+
+
